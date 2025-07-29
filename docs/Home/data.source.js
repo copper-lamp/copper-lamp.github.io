21b129b930f95087ead8779cd81624fc7a93f671
Magicobs0z <146667696+magicobs0z@users.noreply.github.com>
2025-07-29 10:51:22 +0800

    Add files via upload

---
+++ docs/Home/data.source.js
@@ -0,0 +1,551 @@
+import React from 'react';
+export const Banner20DataSource = {
+  wrapper: { className: 'banner2' },
+  BannerAnim: {
+    children: [
+      {
+        name: 'elem0',
+        BannerElement: { className: 'banner-user-elem' },
+        page: { className: 'home-page banner2-page' },
+        textWrapper: { className: 'banner2-text-wrapper' },
+        bg: { className: 'bg bg0 mdn0jjcu9o-editor_css' },
+        title: {
+          className: 'banner2-title mdn5i5po1a-editor_css',
+          children: (
+            <span>
+              <p>CopperLamp</p>
+            </span>
+          ),
+        },
+        content: {
+          className: 'banner2-content mdn5hooq5oq-editor_css',
+          children: (
+            <span>
+              <span>
+                <span>
+                  <p>&nbsp;高版本生电一站式解决方案</p>
+                </span>
+              </span>
+            </span>
+          ),
+        },
+        button: {
+          className: 'banner2-button mdmv87eqz1u-editor_css',
+          children: (
+            <span>
+              <p>GitHub</p>
+            </span>
+          ),
+          type: 'primary',
+          href: 'https://github.com/copper-lamp/CopperLamp',
+          target: '_blank',
+        },
+      },
+    ],
+  },
+};
+export const Content130DataSource = {
+  OverPack: {
+    className: 'home-page-wrapper content13-wrapper mdn6q3i9yk-editor_css',
+    playScale: 0.3,
+  },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'image',
+        children:
+          'https://gw.alipayobjects.com/zos/rmsportal/PiqyziYmvbgAudYfhuBr.svg',
+        className: 'title-image',
+      },
+      {
+        name: 'title',
+        children: (
+          <span>
+            <span>
+              <span>
+                <span>
+                  <p>这是什么</p>
+                </span>
+              </span>
+            </span>
+          </span>
+        ),
+        className: 'title-h1',
+      },
+      {
+        name: 'content',
+        children: (
+          <span>
+            Copperlamp 是一个服务于Minecraft
+            基岩版专用服务器(BDS)的插件mod，可以让你更清晰地了解到游戏是如何运行的。
+            简单来说Copperlamp是专为MCBE生电服务的一个BDS mod。
+            它提供了诸如世界运行速度调整，村庄机制可视化，硬编码结构刷怪区域(HSA)可视化，假人，MSPT和TPS查看，实体数量分析，等等功能。<p
+            >
+              <br />
+            </p>
+          </span>
+        ),
+        className: 'title-content',
+      },
+      {
+        name: 'content2',
+        children: (
+          <span>
+            Copperlamp是traptoor的精神继承者，并且加入了空手开盒等实用功能。并提供了GUI使其更加便于使用，搭载的功能都是精简实用的。<p
+            >
+              <br />
+            </p>
+          </span>
+        ),
+        className: 'title-content',
+      },
+    ],
+  },
+};
+export const Feature30DataSource = {
+  wrapper: { className: 'home-page-wrapper content3-wrapper' },
+  page: { className: 'home-page content3 mdn6z45qnc-editor_css' },
+  OverPack: { playScale: 0.3 },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: (
+          <span>
+            <p>整合生电重点需求</p>
+          </span>
+        ),
+        className: 'title-h1',
+      },
+      {
+        name: 'content',
+        className: 'title-content',
+        children: (
+          <span>
+            <p>
+              基于@EOALE studio<span>强大的基础资源</span>
+            </p>
+          </span>
+        ),
+      },
+    ],
+  },
+  block: {
+    className: 'content3-block-wrapper',
+    children: [
+      {
+        name: 'block0',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/ScHBSdwpTkAHZkJ.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: {
+            className: 'content3-title',
+            children: (
+              <span>
+                <p>自由视角</p>
+              </span>
+            ),
+          },
+          content: {
+            className: 'content3-content',
+            children: (
+              <span>
+                <p>切换自由视角，观察机器内部结构</p>
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block1',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/NKBELAOuuKbofDD.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: {
+            className: 'content3-title',
+            children: (
+              <span>
+                <span>
+                  <span>
+                    <p>假人</p>
+                  </span>
+                </span>
+              </span>
+            ),
+          },
+          content: {
+            className: 'content3-content',
+            children: (
+              <span>
+                <p>稳定可靠的轻量化假人，满足一般机器挂机需要</p>
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block2',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/xMSBjgxBhKfyMWX.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: {
+            className: 'content3-title',
+            children: (
+              <span>
+                <p>投影</p>
+              </span>
+            ),
+          },
+          content: {
+            className: 'content3-content',
+            children: (
+              <span>
+                <p>在世界中显示结构，傻瓜式建筑体验</p>
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block3',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/MNdlBNhmDBLuzqp.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: {
+            className: 'content3-title',
+            children: (
+              <span>
+                <p>轻松放置</p>
+              </span>
+            ),
+          },
+          content: {
+            className: 'content3-content',
+            children: (
+              <span>
+                <span>
+                  <p>j解放眼睛，对着投影放置即可，自动识别方块</p>
+                </span>
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block4',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/UsUmoBRyLvkIQeO.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: {
+            className: 'content3-title',
+            children: (
+              <span>
+                <p>村庄可视化</p>
+              </span>
+            ),
+          },
+          content: {
+            className: 'content3-content',
+            children: (
+              <span>
+                <p>显示村庄中心，铁傀儡范围，poi等，方便建设村庄工程</p>
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block5',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/ipwaQLBLflRfUrg.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: {
+            className: 'content3-title',
+            children: (
+              <span>
+                <p>HSA显示</p>
+              </span>
+            ),
+          },
+          content: {
+            className: 'content3-content',
+            children: (
+              <span>
+                <p>找到刷怪点如此简单</p>
+              </span>
+            ),
+          },
+        },
+      },
+    ],
+  },
+};
+export const Banner50DataSource = {
+  wrapper: { className: 'home-page-wrapper banner5' },
+  page: { className: 'home-page banner5-page mdn6vsehsor-editor_css' },
+  childWrapper: {
+    className: 'banner5-title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: (
+          <span>
+            <span>
+              <p>开始使用</p>
+            </span>
+          </span>
+        ),
+        className: 'banner5-title',
+      },
+      {
+        name: 'explain',
+        className: 'banner5-explain',
+        children: (
+          <span>
+            <p>下载适合的版本</p>
+          </span>
+        ),
+      },
+      {
+        name: 'content',
+        className: 'banner5-content',
+        children: (
+          <span>
+            <p>前往仓库页面</p>
+          </span>
+        ),
+      },
+      {
+        name: 'button',
+        className: 'banner5-button-wrapper',
+        children: {
+          href: '#https://github.com/copper-lamp/CopperLamp',
+          className: 'banner5-button',
+          type: 'primary',
+          children: (
+            <span>
+              <p>开源地址</p>
+            </span>
+          ),
+          target: '_blank',
+        },
+      },
+    ],
+  },
+  image: {
+    className: 'banner5-image',
+    children: 'https://pic1.imgdb.cn/item/65dab5939f345e8d03c0482c.jpg',
+  },
+};
+export const Teams10DataSource = {
+  wrapper: { className: 'home-page-wrapper teams1-wrapper' },
+  page: { className: 'home-page teams1' },
+  OverPack: { playScale: 0.3, className: '' },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: (
+          <span>
+            <p>主要维护人员</p>
+          </span>
+        ),
+      },
+    ],
+  },
+  block: {
+    className: 'block-wrapper',
+    children: [
+      {
+        name: 'block0',
+        className: 'block',
+        md: 8,
+        xs: 24,
+        titleWrapper: {
+          children: [
+            {
+              name: 'image',
+              className: 'teams1-image',
+              children:
+                'https://gw.alipayobjects.com/mdn/rms_ae7ad9/afts/img/A*--rVR4hclJYAAAAAAAAAAABjARQnAQ',
+            },
+            {
+              name: 'title',
+              className: 'teams1-title',
+              children: (
+                <span>
+                  <p>芝magicobs0z</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content',
+              className: 'teams1-job',
+              children: (
+                <span>
+                  <p>@eoale studio 项目经理</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content1',
+              className: 'teams1-content',
+              children: (
+                <span>
+                  <p>
+                    村民交易所区人物，基岩版知名机器作者，拥有丰富经验，熟悉软件开发
+                  </p>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+      {
+        name: 'block1',
+        className: 'block',
+        md: 8,
+        xs: 24,
+        titleWrapper: {
+          children: [
+            {
+              name: 'image',
+              className: 'teams1-image',
+              children:
+                'https://pic1.imgdb.cn/item/65b3dfb5871b83018a109c08.jpg',
+            },
+            {
+              name: 'title',
+              className: 'teams1-title',
+              children: (
+                <span>
+                  <p>池鲤chili6668267</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content',
+              className: 'teams1-job',
+              children: (
+                <span>
+                  <p>CLZ生电服务器服主</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content1',
+              className: 'teams1-content',
+              children: (
+                <span>
+                  <p>
+                    扎根Minecraft社区，热线解决各种服务器难题，软件开发者，深入生电一线，知道玩家需求
+                  </p>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+      {
+        name: 'block2',
+        className: 'block',
+        md: 8,
+        xs: 24,
+        titleWrapper: {
+          children: [
+            {
+              name: 'image',
+              className: 'teams1-image',
+              children:
+                'https://pic1.imgdb.cn/item/65b3e01f871b83018a125347.jpg',
+            },
+            {
+              name: 'title',
+              className: 'teams1-title',
+              children: (
+                <span>
+                  <p>hedgehog</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content',
+              className: 'teams1-job',
+              children: (
+                <span>
+                  <p>j技术支持</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content1',
+              className: 'teams1-content',
+              children: (
+                <span>
+                  <p>著名网络全栈工程师，负责网络技术维护与研发</p>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+    ],
+  },
+};
+export const Footer00DataSource = {
+  wrapper: { className: 'home-page-wrapper footer0-wrapper' },
+  OverPack: { className: 'home-page footer0', playScale: 0.05 },
+  copyright: {
+    className: 'copyright',
+    children: (
+      <span>
+        <span>©2025 @copperlamp&nbsp;</span>
+      </span>
+    ),
+  },
+};
